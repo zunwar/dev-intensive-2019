c53f82ef64355c2c18f7c3433969df2aaacd9171
zunwar <chief.agin@yandex.ru>
2019-07-02 17:58:09 +0500

    hometask_2 stas

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -23,6 +23,38 @@ object Utils {
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        return "eshe ne sdelano toInitials"
+        var f : String = ""
+        var l : String = ""
+
+        if (firstName != null && firstName.isNotEmpty()) {
+            val fic = firstName.toCharArray().first()
+            val fi = fic.toString().toUpperCase()
+            f = fi
+        }
+
+        if (lastName != null && lastName.isNotEmpty()) {
+            val lic = lastName.toCharArray().first()
+            val li = lic.toString().toUpperCase()
+            l=li
+        }
+
+        return when {
+            firstName=="" -> (l)
+            lastName=="" -> (f)
+            firstName==null&& lastName==null -> ("null")
+            firstName==" " || lastName==" " -> ("null")
+            else -> ("$f$l")
+        }
+
+//        return "null"
+
+//        return when {
+//            firstName=="" -> null
+//            else -> val fi: Char =name?.first()
+//        }
+
+//        var fi = firstName?.get(0)
+//        var si = lastName?.get(0)
+//        return println("$fi , $si")
     }
 }
