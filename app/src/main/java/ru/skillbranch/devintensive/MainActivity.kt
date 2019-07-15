86266dc109ad40d49abd2e92458d337bd7ea6104
zunwar <chief.agin@yandex.ru>
2019-07-15 17:43:39 +0500

    hometask_3 layouts

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,12 +1,14 @@
 package ru.skillbranch.devintensive
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("M_MainActivity", "OnCreate")
     }
 }
