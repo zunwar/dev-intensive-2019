3cab02d0bf4412844b5cb7963a1422e2a299375a
zunwar <chief.agin@yandex.ru>
2019-07-16 17:43:28 +0500

    hometask_3 nspp

--- app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/MainActivity.kt
@@ -1,14 +1,68 @@
 package ru.skillbranch.devintensive
 
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.skillbranch.devintensive.models.Bender
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+    lateinit var benderImage: ImageView
+    lateinit var textTxt : TextView
+    lateinit var messageEt : EditText
+    lateinit var sendBtn : ImageView
+    lateinit var benderObj : Bender
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("M_MainActivity", "OnCreate")
+
+//        benderImage = findViewById(R.id.iv_bender)
+        benderImage = iv_bender
+        textTxt = tv_text
+        messageEt = et_message
+        sendBtn = iv_send
+
+        val status = savedInstanceState?.getString("STATUS") ?: Bender.Status.NORMAL.name
+        val question = savedInstanceState?.getString("QUESTION") ?: Bender.Question.NAME.name
+//        val prevtext = savedInstanceState?.getString("text") ?: messageEt.toString()
+        benderObj = Bender(Bender.Status.valueOf(status), Bender.Question.valueOf(question))
+
+        Log.d("M_MainActivity", "OnCreate $status $question")
+
+        val (r,g,b) = benderObj.status.color
+        benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+
+        textTxt.text = (benderObj.askQuestion())
+//        messageEt.setText(prevtext)
+
+        sendBtn.setOnClickListener(this)
+    }
+
+    override  fun onClick(v:View?) {
+        if(v?.id == R.id.iv_send) {
+           val (phrase, color) = benderObj.listenAnswer(messageEt.text.toString().toLowerCase())
+            messageEt.setText("")
+            val (r,g,b) = color
+            benderImage.setColorFilter(Color.rgb(r,g,b), PorterDuff.Mode.MULTIPLY)
+            textTxt.text = phrase
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putString("STATUS" , benderObj.status.name)
+        outState?.putString("QUESTION" , benderObj.question.name)
+//        outState?.putString("text" , messageEt.toString())
+        Log.d("M_MainActivity" , "onSaveInstanceState ${benderObj.status.name}   ${benderObj.question.name}")
+
     }
 }
---
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -0,0 +1,89 @@
+package ru.skillbranch.devintensive.models
+
+class Bender (var status: Status = Status.NORMAL, var question : Question = Question.NAME) {
+
+    fun askQuestion(): String = when (question) {
+        Question.NAME -> Question.NAME.question
+        Question.PROFESSION -> Question.PROFESSION.question
+        Question.MATERIAL -> Question.MATERIAL.question
+        Question.BDAY -> Question.BDAY.question
+        Question.SERIAL -> Question.SERIAL.question
+        Question.IDLE -> Question.IDLE.question
+    }
+
+    fun listenAnswer(answer: String): Pair<String, Triple<Int, Int, Int>> {
+        return if (question.answers.contains(answer)) {
+            question = question.nextQuestion()
+            "Отлично - это правильный ответ!\n${question.question}" to status.color
+        } else {
+            status = status.nextStatus()
+            "Это неправильный ответ!\n${question.question}" to status.color
+        }
+    }
+
+    enum class Status(val color: Triple<Int, Int, Int>) {
+        NORMAL(Triple(255, 255, 255)),
+        WARNING(Triple(255, 120, 0)),
+        DANGER(Triple(255, 60, 60)),
+        CRITICAL(Triple(255, 0, 0));
+
+        fun nextStatus(): Status {
+            return if (this.ordinal < values().lastIndex) {
+                values()[this.ordinal + 1]
+            } else {
+                values()[0]
+            }
+        }
+
+//        enum class Question(val question: String, val answers: List<String>) {
+//            NAME("Как меня зовут?", listOf("бендер", "bender")),
+//            PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")),
+//            MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")),
+//            BDAY("Когда меня создали?", listOf("2993")),
+//            SERIAL("Мой серийный номер?", listOf("2716057")),
+//            IDLE("На этом все, вопросов больше нет", listOf())
+//        }
+
+    }
+
+
+    enum class Question(val question: String, val answers: List<String>) {
+        NAME("Как меня зовут?", listOf("бендер", "bender")) {
+            override fun nextQuestion(): Question = PROFESSION
+        },
+        PROFESSION("Назови мою профессию?", listOf("сгибальщик", "bender")) {
+            override fun nextQuestion(): Question = MATERIAL
+        },
+        MATERIAL("Из чего я сделан?", listOf("металл", "дерево", "metal", "iron", "wood")) {
+            override fun nextQuestion(): Question = BDAY
+        },
+        BDAY("Когда меня создали?", listOf("2993")) {
+            override fun nextQuestion(): Question = SERIAL
+        },
+        SERIAL("Мой серийный номер?", listOf("2716057")) {
+            override fun nextQuestion(): Question = IDLE
+        },
+        IDLE("На этом все, вопросов больше нет", listOf()) {
+            override fun nextQuestion(): Question = IDLE
+        };
+
+        abstract fun nextQuestion(): Question
+
+    }
+}
+
+//    enum class Quusetion
+
+    //Валидация
+
+//    Question.NAME -> "Имя должно начинаться с заглавной буквы"
+//
+//    Question.PROFESSION -> "Профессия должна начинаться со строчной буквы"
+//
+//    Question.MATERIAL -> "Материал не должен содержать цифр"
+//
+//    Question.BDAY -> "Год моего рождения должен содержать только цифры"
+//
+//    Question.SERIAL -> "Серийный номер содержит только цифры, и их 7"
+//
+//    Question.IDLE -> //игнорировать валидацию
