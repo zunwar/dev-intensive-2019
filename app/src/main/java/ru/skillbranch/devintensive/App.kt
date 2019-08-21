0cb5b275db9205647a7a8599f1fbf53e1d54ab7b
zunwar <chief.agin@yandex.ru>
2019-08-21 17:57:46 +0500

    hometask_4  splashtheme

--- app/src/main/java/ru/skillbranch/devintensive/App.kt
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -22,6 +22,7 @@ class App : Application() {
 
 
     override fun onCreate() {
+        setTheme(R.style.AppTheme)
         super.onCreate()
         PreferencesRepository.getAppTheme().also {
             AppCompatDelegate.setDefaultNightMode(it)
